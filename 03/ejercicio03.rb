445dacc203f3320feebae5068961c65313138858
belenac <eac1992@gmail.com>
2018-05-02 19:54:06 -0300

    Ejercicio 03 part 1

---
+++ 03/ejercicio03.rb
@@ -0,0 +1,14 @@
+h = { 'x' => 1, 'y' => 2 }
+
+# Agregar el string z con el valor 3.
+h['z'] = 3
+puts h
+
+# Cambiar el valor de x por 5.
+
+# Eliminar la clave y.
+
+# Si el hash tiene una clave llamada z mostrar en pantalla "yeeah".
+
+# Invertir el diccionario de forma que los valores sean las llaves y las llaves los valores
+
