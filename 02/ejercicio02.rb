407f1b569d5842b78de04a1583c5d1f2af62c14f
belenac <eac1992@gmail.com>
2018-05-02 19:35:26 -0300

    Ejercicio 02 part 2

--- 02/ejercicio02.rb
+++ 02/ejercicio02.rb
@@ -1,2 +1,6 @@
 productos = { 'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750 }
 productos.each { |valor, producto| puts producto }
+
+productos['cereal'] = 2200
+
+puts productos
