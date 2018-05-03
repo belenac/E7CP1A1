f7647195ee4e5f3ae907fb8467bd278e49cd7813
belenac <eac1992@gmail.com>
2018-05-02 21:07:31 -0300

    Ejercicio 04 part 2

--- 04/ejercicio04.rb
+++ 04/ejercicio04.rb
@@ -4,3 +4,12 @@ edades = [32, 28, 41, 19]
 personas_hash = personas.zip(edades)
 
 print personas_hash.to_h
+
+def promedio(num_prom)
+  prom = 0
+  num_prom.each do |key, value|
+    prom += value
+  end
+  prom / 4
+end
+puts promedio(personas_hash)
